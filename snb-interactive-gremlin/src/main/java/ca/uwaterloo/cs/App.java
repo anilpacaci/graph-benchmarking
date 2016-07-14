2313fe63e57849d537d49f5aca19fae5d9307294
Anil Pacaci <apacaci@uwaterloo.ca>
2016-07-14 18:13:00 -0400

    maven project structure

---
+++ snb-interactive-gremlin/src/main/java/ca/uwaterloo/cs/App.java
@@ -0,0 +1,13 @@
+package ca.uwaterloo.cs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
