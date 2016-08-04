91d9ac0c06f96603093030bb945b51105f320621
Anil Pacaci <apacaci@uwaterloo.ca>
2016-08-03 22:33:28 -0400

    Dummy implementation for Complex 1, 10 and 14

---
+++ snb-interactive-gremlin/src/main/java/ca/uwaterloo/cs/ldbc/interactive/gremlin/handler/LdbcComplexQuery1Handler.java
@@ -0,0 +1,31 @@
+package ca.uwaterloo.cs.ldbc.interactive.gremlin.handler;
+
+import ca.uwaterloo.cs.ldbc.interactive.gremlin.Entity;
+import ca.uwaterloo.cs.ldbc.interactive.gremlin.GremlinDbConnectionState;
+import ca.uwaterloo.cs.ldbc.interactive.gremlin.GremlinUtils;
+import com.ldbc.driver.DbConnectionState;
+import com.ldbc.driver.DbException;
+import com.ldbc.driver.OperationHandler;
+import com.ldbc.driver.ResultReporter;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcNoResult;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery1;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery2;
+import com.ldbc.driver.workloads.ldbc.snb.interactive.LdbcQuery2Result;
+import org.apache.tinkerpop.gremlin.driver.Client;
+import org.apache.tinkerpop.gremlin.driver.Result;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.VertexProperty;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class LdbcComplexQuery1Handler implements OperationHandler<LdbcQuery1, DbConnectionState> {
+    @Override
+    public void executeOperation(LdbcQuery1 ldbcQuery1, DbConnectionState dbConnectionState, ResultReporter resultReporter) throws DbException {
+
+        resultReporter.report(0, null, ldbcQuery1);
+    }
+}
