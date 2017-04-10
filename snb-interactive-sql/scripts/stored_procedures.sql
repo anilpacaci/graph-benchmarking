f3a86dbf83f79a8d3a822bd4b38fcbf433f23f05
Anil Pacaci <apacaci@uwaterloo.ca>
2017-04-09 21:45:34 -0400

    Stored procedures for post_views

--- snb-interactive-sql/scripts/stored_procedures.sql
+++ snb-interactive-sql/scripts/stored_procedures.sql
@@ -1,33 +1,59 @@
-create procedure post_view_1 (in postid int) {
-  declare content, imagefile varchar;
-  declare creationdate int;
-  result_names(content, imagefile, creationdate);
-
-  whenever not found goto done1;
-  declare cr1 cursor for 
-      select ps_content, ps_imagefile, ps_creationdate
-        from post
-	where
-	  ps_postid = postid;
-
-  open cr1;
-  while (1)
-    {
-      fetch cr1 into content, imagefile, creationdate;
-      result (content, imagefile, creationdate);
-    }
-
-done1:
-  close cr1;
-}
-
-
-create function post_view_1(postid int)
-declare
-    content varchar;
-    imagefile varchar;
-    creationdate int;
-    result_names(content, imagefile, creationdate);
+create or replace function post_view_1(postid bigint)
+returns table (content varchar, imagefile varchar, creationdate bigint) as $func$
 begin
-
+    return query
+    select ps_content, ps_imagefile, ps_creationdate from post where ps_postid = postid;
 end
+$func$ LANGUAGE plpgsql;
+
+create or replace function post_view_2(postid bigint)
+returns table (personid bigint, firstname varchar, lastname varchar) as $func$
+begin
+    return query
+    select p_personid, p_firstname, p_lastname from post, person where ps_postid = postid and ps_creatorid = p_personid;
+end
+$func$ LANGUAGE plpgsql;
+
+
+create or replace function post_view_3(postid bigint)
+returns table (forumid bigint, forumname varchar, modpersonid bigint, modfirstname varchar, modlastname varchar) as $func$
+begin
+    return query
+    select f_forumid, f_title, p_personid, p_firstname, p_lastname
+        from post ps, person p, forum f
+	where
+	  ps.ps_postid = (
+	      with recursive reply_closure(id, parentid) as (
+            select ps_postid, ps_replyof
+            from post
+            where ps_postid = postid
+            union all
+            select ps_postid, ps_replyof
+            from reply_closure, post
+            where parentid = ps_postid
+          )
+	      select id from reply_closure where parentid is null
+	  )
+	  and ps.ps_forumid = f.f_forumid and f.f_moderatorid = p.p_personid;
+end
+$func$ LANGUAGE plpgsql;
+
+
+create or replace function post_view_4(postid bigint)
+returns table (origpostid bigint, origpostcontent varchar, creationdate bigint, origauthorid bigint, origfirstname varchar, origlastname varchar, friendornot boolean) as $func$
+begin
+    return query
+    select p2.ps_postid, p2.ps_content, p2.ps_creationdate, p_personid, p_firstname, p_lastname,
+      	     (case when exists (
+	     	   	       select 1 from knows
+			       where p1.ps_creatorid = k_person1id and p2.ps_creatorid = k_person2id)
+	      then true
+	      else false
+	      end)
+        from post p1, post p2, person p
+	where
+	  p1.ps_postid = postid and p2.ps_replyof = p1.ps_postid and p2.ps_creatorid = p.p_personid
+	order by 3 desc, 4;
+end
+$func$ LANGUAGE plpgsql;
+
