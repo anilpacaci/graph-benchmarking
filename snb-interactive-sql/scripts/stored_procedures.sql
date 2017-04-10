1a848fd40b2ceb1e1a0d1398d8812a6e14cca3ca
Anil Pacaci <apacaci@uwaterloo.ca>
2017-04-10 19:47:56 -0400

    Short Queries are completed

--- snb-interactive-sql/scripts/stored_procedures.sql
+++ snb-interactive-sql/scripts/stored_procedures.sql
@@ -57,3 +57,51 @@ begin
 end
 $func$ LANGUAGE plpgsql;
 
+
+create or replace function person_view_1(personid bigint)
+returns table (firstname varchar, lastname varchar, gender varchar, birthday bigint, creatindate bigint, locationip varchar, browserused varchar, placeid bigint) as $func$
+begin
+    return query
+    select p_firstname, p_lastname, p_gender, p_birthday, p_creationdate, p_locationip, p_browserused, p_placeid
+    from person
+    where p_personid = personid;
+end
+$func$ LANGUAGE plpgsql;
+
+create or replace function person_view_2(personid bigint)
+returns table (postid bigint, content varchar, imagefile varchar, postcreationdate bigint, origpostid bigint, origpersonid bigint, origfirstname varchar, origlastname varchar) as $func$
+begin
+    return query
+    select ps.ps_postid, ps.ps_content, ps.ps_imagefile, ps.ps_creationdate, ops.ps_postid, p.p_personid, p.p_firstname, p.p_lastname
+    from post ps, post ops, person p
+    where ps.ps_creatorid = personid and
+    ops.ps_postid = (
+	      with recursive reply_closure(id, parentid) as (
+            select ps_postid, ps_replyof
+            from post
+            where ps_postid = ps.ps_postid
+            union all
+            select ps_postid, ps_replyof
+            from reply_closure, post
+            where parentid = ps_postid
+          )
+	      select id from reply_closure where parentid is null
+	  ) and
+	  ops.ps_creatorid = p.p_personid
+    order by ps.ps_creationdate desc
+    limit 10;
+end
+$func$ LANGUAGE plpgsql;
+
+
+create or replace function person_view_3(personid bigint)
+returns table (friendpersonid bigint, friendfirstname varchar, friendlastname varchar, since bigint) as $func$
+begin
+    return query
+    select p.p_personid, p.p_firstname, p.p_lastname, k.k_creationdate
+        from knows k, person p
+	where
+	  k.k_person1id = personid and k.k_person2id = p.p_personid
+	order by 4 desc, 1;
+end
+$func$ LANGUAGE plpgsql;
